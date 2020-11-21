6ea986ba2ed38c65ab6d5f1f4b55f8880cb809d1
Yatharth-Damahe <66172099+Yatharth-Damahe@users.noreply.github.com>
2020-11-21 15:28:11 +0530

    first commit

---
+++ pay.php
@@ -0,0 +1,40 @@
+<?php 
+$product_name = $_POST["product_name"];
+$price = $_POST["amount"];
+$name = $_POST["name"];
+$phone = $_POST["phone"];
+$email = $_POST["email"];
+
+
+include 'src/instamojo.php';
+
+$api = new Instamojo\Instamojo('test_78ad5ebb0e3bf79844cf54e8c30', 'test_c5462f78c8ccbfaf2d900e5d7b2','https://test.instamojo.com/api/1.1/');
+
+
+try {
+    $response = $api->paymentRequestCreate(array(
+        "purpose" => $product_name,
+        "amount" => $price,
+        "buyer_name" => $name,
+        "phone" => $phone,
+        "send_email" => true,
+        "send_sms" => true,
+        "email" => $email,
+        'allow_repeated_payments' => false,
+        "redirect_url" => "http://angelsonearth.000webhostapp.com//instamojo/thankyou.php",
+        "webhook" => "http://angelsonearth.000webhostapp.com//instamojo/webhook.php"
+        ));
+    //print_r($response);
+
+    $pay_ulr = $response['longurl'];
+    
+    //Redirect($response['longurl'],302); //Go to Payment page
+
+    header("Location: $pay_ulr");
+    exit();
+
+}
+catch (Exception $e) {
+    print('Error: ' . $e->getMessage());
+}     
+  ?>
